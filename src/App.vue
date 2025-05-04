6cfef4b3c5c625091e288eff65dbbba9ebad0b2c
Jordan M <johan500050@gmail.com>
2025-05-03 21:40:14 -0400

    Frontend commit 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <div class="banner">
+      <h1>Gestión de calidad de sistemas</h1>
+      <div class="button-row">
+        <router-link class="button" to="/pagina1">Commits</router-link>
+      </div>
+    </div>
+    <router-view />
+  </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/* Estilos para el banner o navbar */
+
+#app{
+  background-color: #f5e5dc;
+  margin: -3px;
+  padding:10px;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+.banner {
+  background-color: #007bff;
+  color: white;
+  margin: -20px;
+  padding: 5px;
   text-align: center;
-  color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.banner h1 {
+  margin: 0;
+  font-size: 2em;
+}
+.banner h3 {
+  margin: 0;
+  justify-content: left;
+  font-size: 1em;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+/* Estilos para la fila de botones */
+.button-row {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.button {
+  text-decoration: none;
+  color: white;
+  background-color: #28a745;
+  padding: 15px 30px;
+  margin: 10px;
+  border-radius: 5px;
+  font-size: 1.2em;
+}
+
+.button:hover {
+  background-color: #218838;
 }
 </style>
---
+++ src/views/Pagina1.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Primer Commit - Jordan : 03/3/05</h2> 
+  </div>
+</template>
+
