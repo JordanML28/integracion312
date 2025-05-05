800f3989a9870de90e0c307ffca917d7570832f0
jackelinss <124317565+jackelinss@users.noreply.github.com>
2025-05-05 16:38:23 -0600

    Segundo commit

--- src/views/Pagina1.vue
+++ src/views/Pagina1.vue
@@ -4,6 +4,7 @@
     <h2>Segundo Commit - Jordan : 05/05/25</h2> 
     <h2>Primer Commit - Jackelin :05/05/25</h2>
     <h2>Tercer Commit - Jordan : 05/05/25 18:31</h2>
+     <h2>Segundo Commit - Jackelin :05/05/25 18:37</h2>
   </div>
 </template>
 
