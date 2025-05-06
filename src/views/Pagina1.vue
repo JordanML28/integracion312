0526721215558f34a98d4ea7a9c9adeb5c98853b
jackelinss <124317565+jackelinss@users.noreply.github.com>
2025-05-06 11:36:24 -0600

    Cuarto commit Jackelin

--- src/views/Pagina1.vue
+++ src/views/Pagina1.vue
@@ -9,6 +9,7 @@
     <h2>Cuarto Commit - Jordan : 06/05/25 11:54</h2>
     <h2>Quinto Commit - Jordan : 06/05/25 12:00</h2>
     <h2>Sexto Commit - Jordan : 06/05/25 12:02</h2>
+    <h2>SCuarto Commit-Jackelin:06/05/25 13:35<h2>
   </div>
 </template>
 
