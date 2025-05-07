3f3482537fa5c3c687119cf41df652cef3979ecd
Jordan M <johan500050@gmail.com>
2025-05-07 19:09:54 -0400

    Ultima Prueba commit Jordan

--- src/views/Pagina1.vue
+++ src/views/Pagina1.vue
@@ -4,7 +4,7 @@
     <h2>Segundo Commit - Jordan : 05/05/25</h2> 
     <h2>Primer Commit - Jackelin :05/05/25</h2>
     <h2>Tercer Commit - Jordan : 05/05/25 18:31</h2>
-     <h2>Segundo Commit - Jackelin :05/05/25 18:37</h2>
+    <h2>Segundo Commit - Jackelin :05/05/25 18:37</h2>
     <h2>Tercer Commit - Jackelin :05/05/25 18:40</h2>
     <h2>Cuarto Commit - Jordan : 06/05/25 11:54</h2>
     <h2>Quinto Commit - Jordan : 06/05/25 12:00</h2>
@@ -17,10 +17,10 @@
     <h2>Octavo Commit - Jordan : 07/05/25 12:38</h2> 
     <h2>Noveno Commit - Jordan : 07/05/25 12:40</h2> 
     <h2>Decimo Commit - Jordan : 07/05/25 12:45</h2> 
-    <h2>Ultima prueba - Jordan : 07/05/25 19:02</h2> 
-     <h2>Octavo Commit - Jackelin : 07/05/25 14:14</h2> 
-     <h2>Noveno Commit - Jackelin : 07/05/25 14:16</h2> 
-     <h2>Decimo Commit - Jackelin : 07/05/25 14:21</h2> 
+    <h2>Octavo Commit - Jackelin : 07/05/25 14:14</h2> 
+    <h2>Noveno Commit - Jackelin : 07/05/25 14:16</h2> 
+    <h2>Decimo Commit - Jackelin : 07/05/25 14:21</h2> 
+    <h2>Ultima prueba - Jordan : 07/05/25 19:09</h2> 
   </div>
 </template>
 
